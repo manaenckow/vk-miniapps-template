ee012fba39461f8aeb528f01690aad4354119d38
Maxim Manaenkov <manaenkov2000@gmail.com>
2020-04-25 01:47:47 +0300

    eslint fixes

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import {
-	Panel, PanelHeaderSimple, Button, Group, Div, Header, FormLayout
+  Panel,
+  PanelHeaderSimple,
+  Button,
+  Group,
+  Div,
+  Header,
+  FormLayout
 } from '@vkontakte/vkui';
 
 const Home = props => {
+  const { fetchedUser } = props.state;
 
-	const { fetchedUser } = props.state;
+  return (
+    <Panel id={props.id}>
+      <PanelHeaderSimple>Example</PanelHeaderSimple>
 
-	return (
-		<Panel id={props.id}>
-			<PanelHeaderSimple>Example</PanelHeaderSimple>
-
-			<Group header={<Header>Basic functions</Header>}>
-				<Div>{`${fetchedUser.first_name} ${fetchedUser.last_name}`}</Div>
-				<Div>
-					<FormLayout>
-						<Button size="xl" onClick={() => props.go('panel2')}>
-							Simple button
-						</Button>
-						<Button size="xl" onClick={() => props.openDoneSnackBar('Done.')} >
-							OpenDoneSnackBar
-						</Button>
-						<Button size="xl" onClick={() => props.openErrorSnackBar('Unknown error occurred.')} >
-							OpenErrorSnackBar
-						</Button>
-						<Button size="xl" onClick={() => props.openModal('main')} >
-							Open simple modal
-						</Button>
-					</FormLayout>
-				</Div>
-			</Group>
-			{props.state.snackbar}
-		</Panel>
-	);
+      <Group header={<Header>Basic functions</Header>}>
+        <Div>{`${fetchedUser.first_name} ${fetchedUser.last_name}`}</Div>
+        <Div>
+          <FormLayout>
+            <Button size="xl" onClick={() => props.go('panel2')}>
+              Simple button
+            </Button>
+            <Button size="xl" onClick={() => props.openDoneSnackBar('Done.')}>
+              OpenDoneSnackBar
+            </Button>
+            <Button
+              size="xl"
+              onClick={() => props.openErrorSnackBar("Unknown error occurred.")}
+            >
+              OpenErrorSnackBar
+            </Button>
+            <Button size="xl" onClick={() => props.openModal('main')}>
+              Open simple modal
+            </Button>
+          </FormLayout>
+        </Div>
+      </Group>
+      {props.state.snackbar}
+    </Panel>
+  );
 };
 
 export default Home;
--- src/panels/Panel2.js
+++ src/panels/Panel2.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Panel, PanelHeaderSimple, PanelHeaderBack } from '@vkontakte/vkui';
 
 const Panel2 = props => (
-	<Panel id={props.id}>
-		<PanelHeaderSimple
-			left={<PanelHeaderBack onClick={() => props.go('home')}/>}
-		>
-			Example
-		</PanelHeaderSimple>
-	</Panel>
+  <Panel id={props.id}>
+    <PanelHeaderSimple
+      left={<PanelHeaderBack onClick={() => props.go('home')} />}
+    >
+      Example
+    </PanelHeaderSimple>
+  </Panel>
 );
 
 export default Panel2;
